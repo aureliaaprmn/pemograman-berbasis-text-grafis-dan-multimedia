6d901d187640cf5ff3374f0ee3c186b4977d5cdc
aureliaaprmn <144987491+aureliaaprmn@users.noreply.github.com>
2024-07-27 21:24:08 +0700

    Add files via upload

---
+++ Bab2soal1java/src/bab2soal1java/Bab2soal1java.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package bab2soal1java;
+
+/**
+ *
+ * @author Sheva
+ */
+public class Bab2soal1java {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int a = 5;
+        int b = 45;
+        int c = a + b;
+        System.out.println(c); 
+    }
+}
